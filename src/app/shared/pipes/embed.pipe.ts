7b1c51eb9a8459776866cf6dc4a29119277e8f50
NaianeReis27 <email.annereis@gmail.com>
2023-05-18 23:59:53 -0300

    feat: update styles

--- src/app/shared/pipes/embed.pipe.ts
+++ src/app/shared/pipes/embed.pipe.ts
@@ -1,17 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
-
 @Pipe({
-  name: 'embed'
+  name: 'embed',
 })
-
 export class EmbedPipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer) {}
   transform(value: SafeUrl, ...args: unknown[]): SafeUrl {
-    const string = value.toString()
-    const valid = string.split('watch?v=')[1].substring(0, 11)
-    const video = `https://www.youtube.com/embed/${valid}?version=3&enablejsapi=1`
-    return this.sanitizer.bypassSecurityTrustResourceUrl(video)
+    const string = value.toString();
+    const valid = string.split('watch?v=')[1].substring(0, 11);
+    const video = `https://www.youtube.com/embed/${valid}?version=3&enablejsapi=1`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(video);
   }
 }
--- src/app/shared/pipes/thumb.pipe.ts
+++ src/app/shared/pipes/thumb.pipe.ts
@@ -1,14 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'thumb'
+  name: 'thumb',
 })
 export class ThumbPipe implements PipeTransform {
-
-  transform(value: string, ...args: unknown[]): string {
-    let valid = value.split('watch?v=')[1].substring(0, 11)
-    const imageThumb = `https://img.youtube.com/vi/${valid}/hqdefault.jpg`
-    return imageThumb
+  transform(value: SafeUrl, ...args: unknown[]): string {
+    let valid = value.toString().split('watch?v=')[1].substring(0, 11);
+    const imageThumb = `https://img.youtube.com/vi/${valid}/hqdefault.jpg`;
+    return imageThumb;
   }
-
 }
